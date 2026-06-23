8714953b49d435bd7ca2b0cb30b34f70a069b09b
Kareeeels <zs24021118@estudiantes.uv.mx>
2026-06-23 11:09:23 -0600

    Remove backend again

--- backend/src/main/java/com/stockpulse/repository/StockRepository.java
+++
@@ -1,7 +0,0 @@
-package com.stockpulse.repository;
-
-import com.stockpulse.entity.Stock;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StockRepository extends JpaRepository<Stock, Long> {
-}
